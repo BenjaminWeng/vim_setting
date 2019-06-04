ff131ae8d2c441e4d6f2c84d1acfcd39ca2748a5
benjamin <benjamin.weng@kneron.us>
2019-06-04 17:56:53 +0800

    1. add script to install vim packs. 2. modify readme. 3. NerdTree default off.

--- README.md
+++ README.md
@@ -6,10 +6,10 @@ https://juejin.im/post/5b597a9cf265da0f9402b434
 2. copy .vimrc  
 2.5 YCM  
 cd ~/.vim/bundle  
-git clone https://github.com/Valloric/YouCompleteMe.git  
+git clone https://github.com/Valloric/YouCompleteMe.git  
 cd ~/.vim/bundle/YouCompleteMe  
-git submodule update —init --recursive  
-./install.py —all  
+git submodule update --init --recursive  
+./install.py --all  
 3. Install Plugins:  
 sudo vim +PluginInstall +qall  
 4. sudo apt-get install cscope  
---
+++ install.sh
@@ -0,0 +1,20 @@
+#cscope/ctags
+sudo apt-get install cscope  
+sudo apt-get install ctags
+
+# vim_setting
+#1. Set up Vundle:  
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim  
+#1.5 Set up YCM  
+#https://juejin.im/post/5b597a9cf265da0f9402b434  
+#2. copy .vimrc
+cp .vimrc ~/.vimrc
+#2.5 YCM  
+cd ~/.vim/bundle  
+git clone https://github.com/Valloric/YouCompleteMe.git  
+cd ~/.vim/bundle/YouCompleteMe  
+git submodule update --init --recursive  
+./install.py --all
+#3. Install Plugins:  
+sudo vim +PluginInstall +qall
+git config --global core.editor vim
